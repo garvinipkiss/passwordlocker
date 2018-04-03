f09ecc5ddf90d40bd0406d69e6dc670da8e5bb17
garvinipkiss <garvinipkiss@gmail.com>
2018-04-03 07:42:43 +0300

    if statement function

--- run.py
+++ run.py
@@ -129,3 +129,96 @@ def main():
 
             print("Enter last name -")
             l_name = input()
+    print("""
+                  We can generate a password for you. Use the following keys:
+                  generate- to generate a password
+                  open- to create your own password.
+                  """)
+            pass_code = input().lower()
+            print("__" * 20)
+            if pass_code == "generate":
+                password_length = int(
+                    input("create a strong length of your own "))
+                password = password_gen(password_length)
+                print(f"Your new password is {password}")
+            else:
+                print("Write a password that we will save for you")
+                password = input()
+
+            save_users(create_user(f_name, l_name, password))
+
+            print("")
+            print(
+                f"""New account created for - {f_name} {l_name} your account password is - {password}""")
+            print("")
+            print("Use a good password")
+        while True:
+            print("""Use the following keys:
+                  creates - create password account,
+                  display - displays accounts,
+                  find - find a password account,
+                  exit - exit account list.""")
+            short_code = input().lower()
+            print("_" * 100)
+            if key == "creates":
+                print(
+                    "Which accounts do you want to save a password for? Eg Emails, social media, ")
+                account_name = input()
+                print("""
+                      We can generate a passwords for you. Use the following keys:
+                      generate- to generate a password
+                      open- to create your own password.
+                      """)
+                pass_code = input().lower()
+                print("__" * 20)
+                if pass_code == "generate":
+
+    password_length = int(
+                        input("How long do you want your password - "))
+
+                    password = password_gen(password_length)
+                    print("")
+                    print(f"Your password for this {account_name} is: {password}")
+                    print("")
+                    print("__" * 20)
+                else:
+                    print("Write a password of your own. We'll store it for you")
+                    password = input()
+                    password_length = len(password)
+                    print("")
+                    print(f"Your password for this {account_name} is: {password}")
+                    print("")
+                    print("__" * 20)
+
+                save_Accounts(create_account(
+                    account_name, password, password_length))
+
+            elif key == "display":
+                if display_Accounts():
+                    print("Your various accounts")
+                    print("")
+                    for Accounts in display_accounts():
+                        print(
+                            f"Account- {Accounts.account_name}, Password- {Accounts.account_password}, Password Length- {Accounts.password_length}")
+                        print("")
+                else:
+                    print("")
+                    print("No account is yet saved")
+                    print("")
+
+            elif key == "find":
+                print("in order to view your accounts password enter the accounts name")
+
+                account_name = input()
+                if Accounts_exists(account_name):
+                    search_Accounts = find_Accounts(account_name)
+                    print("_" * 20)
+                    print(f"{search_Accounts.account_name}")
+                    print(f"Password - {search_Accounts.account_password}")
+                    print("")
+                    print(
+                        f"Password Length - {search_account.password_length}")
+                    print("_" * 20)
+                else:
+                    print("This account doesn't exist")
+                    print("")
